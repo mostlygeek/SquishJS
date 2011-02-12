c8eda8e224885812d24eb533fca86b6646c194ec
Benson Wong <mostlygeek@gmail.com>
2011-02-12 09:19:01 -0800

    first working version, still really hacky

--- js-shrink/index.php
+++ js-shrink/index.php
@@ -6,6 +6,7 @@ header('Content-type: text/javascript');
 $now = new DateTime('now', new DateTimeZone('UTC'));
 $messages = array('Generated: '.$now->format(DATE_ISO8601));
 
+$errorHttpCode = 400; 
 $errors = array();
 $source = ''; 
 
@@ -27,14 +28,22 @@ if ($host == '') {
  */
  
 $uri = $_SERVER['REQUEST_URI'];
-$fetchQueue = decodeUri($uri, 'js');
+$parts = parse_url($uri);
 
-if (empty($fetchQueue)) {
+
+// @TODO do some mungling with the URI to get only the parts
+// we need.
+$uri = $parts['path'];
+
+
+$files = decodeUri($uri, 'js');
+
+if (empty($files)) {
     $errors[] = 'Nothing to fetch';
     goto ERROR; 
 }
 
-foreach ($fetchQueue as $file) {
+foreach ($files as $file) {
     if (substr($file, -3) != '.js') {
         $errors[] = "$file does not end in .js"; 
     }
@@ -46,8 +55,80 @@ if (count($errors)) {
 
 // STEP 3: fetch the files remotely (curl-multi-get)
 
+$uriQueue = array();
+foreach ($files as $file) {
+    $uriQueue[] = 'https://'.$host.$file; 
+}
 
-// check that there were no fetch errors
+$mh = curl_multi_init();
+$handles = array();
+foreach ($uriQueue as $uriToFetch) {
+    // create a new cURL resource
+    $ch = curl_init();
+
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, $uriToFetch);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    if (curl_multi_add_handle($mh, $ch) !== 0) {
+        die('multi add failed');
+    } else {
+        $handles[] = $ch; // we need these to get the content later
+    }
+}
+
+$start = microtime(true);
+do {
+    usleep(10000); // 10ms
+    curl_multi_exec($mh, $running);
+} while($running > 0);
+
+$total = round(microtime(true) - $start, 4);
+
+foreach ($handles as $ch) {
+    $source .= curl_multi_getcontent( $ch );
+    curl_multi_remove_handle($mh, $ch);
+
+    $info = curl_getinfo($ch);
+    if ($info['http_code'] != '200') {
+        $errors[] = "URI: ".$info['url'].', HTTP: '.$info['http_code'];
+    }
+    curl_close($ch);
+}
+
+if ($errors) {
+    goto ERROR;
+} else {
+    $messages[] = 'Files: '.count($uriQueue);
+    $messages[] = 'Fetch time: '.$total.'s';
+}
+
+/**
+ * STEP 4: Uglify the javascript
+ */
+$descriptor = array(
+    0 => array("pipe", "r"),
+    1 => array("pipe", "w"),
+    2 => array("file", "/tmp/uglify-errors", "a")
+);
+
+$cwd = "/tmp";
+$start = microtime(true);
+$process = proc_open(
+        '/var/web-projects/squishjs/uri-parser/uglifyjs -nc',
+        $descriptor, $pipes, $cwd);
+fwrite($pipes[0], $source);
+fclose($pipes[0]);
+$minimizedSource = stream_get_contents($pipes[1]);
+fclose($pipes[1]);
+$total = round(microtime(true)-$start, 4);
+$messages[] = 'JS Size: '.strlen($minimizedSource);
+/**
+ * Do some error checking here...
+ */
+
+
 // combine all the source files (if multiple)
 // minimize the javascript
 // if errors
@@ -55,14 +136,12 @@ if (count($errors)) {
 //   --
 // return valid, minimized JS. 
 
-goto OUTPUT;
+goto OUTPUT; // skip ERROR output
 
 /*** START:: ERROR */
 ERROR:
-    // see: http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
     header("HTTP/1.0 400 Bad Request");
-    header("Status: 400 Bad Request");
-
+    header("Status: 400 Bad Request"); // for FastCGI
 
 /*** START:: OUTPUT */
 OUTPUT:
@@ -83,12 +162,13 @@ OUTPUT:
 <?php if ($messages) {
     echo $messages."\n"; 
 }
-if ($errors) {
+if ($errors):
     echo " *\n * Errors:\n"; 
-    echo $errors."\n"; 
-}
+    echo $errors."\n";
+endif;
 ?>
-<?php ?>
  */
+<?php if (!$errors) echo $minimizedSource;?>
+
 
 
